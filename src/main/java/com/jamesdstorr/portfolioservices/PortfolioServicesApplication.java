081fa2ab76245df554618c0b5a6997aa3fb559a8
jamesdstorr <jamesstorr@live.co.uk>
2023-11-30 15:12:54 +0000

    Initial commit

---
+++ src/main/java/com/jamesdstorr/portfolioservices/PortfolioServicesApplication.java
@@ -0,0 +1,13 @@
+package com.jamesdstorr.portfolioservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesdstorr/portfolioservices/PortfolioServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesdstorr.portfolioservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
